5dfe83afd894728a679b01dd55a83def6188c44d
connor ng <143060779+connmunist@users.noreply.github.com>
2024-04-21 16:33:08 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -6,10 +6,10 @@ private ArrayList<String> words;
 public RandomStringChooser(String[] wordArray)
 {
   words = new ArrayList<String>();
-  for(string w: wordArray)
+  for(String w: wordArray)
       words.add(w);
 }
-public string getNext(){
+public String getNext(){
 if(words.size() == 0)
     return "NONE";
 int i = (int)(Math.random() * words.size());
